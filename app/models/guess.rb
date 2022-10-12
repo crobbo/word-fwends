28e9d82ce89071e911dfae4e39f677a60edc4e3b
Christian Robinson <christian@robbo.dev>
2022-10-12 23:04:38 +0100

    removed keyboard positioning

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -3,11 +3,11 @@ class Guess < ApplicationRecord
 
   include ActionView::RecordIdentifier
 
-  # after_update_commit do
-  #   broadcast_update_to [game, :guesses], target: "input_#{dom_id(self)}"
+  after_update_commit do
+    broadcast_update_to [game, :guesses], target: "input_#{dom_id(self)}"
 
-  #   broadcast_replace_to [game, :guesses], target: "#{game.id}_key_#{value}",
-  #                                          partial: 'games/key',
-  #                                          locals: { guess: self, game: game, letter: value }
-  # end
+    broadcast_replace_to [game, :guesses], target: "#{game.id}_key_#{value}",
+                                           partial: 'games/key',
+                                           locals: { guess: self, game: game, letter: value }
+  end
 end
